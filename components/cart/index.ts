71999be27e89aeea8fa31be44d5babbc708c942a
duongdo671999@gmail.com <duongdo671999>
2022-12-05 22:58:41 +0700

    final6

--- components/cart/index.ts
+++ components/cart/index.ts
@@ -2,7 +2,7 @@ import { BagItemBtn } from "./BagItemBtn";
 import BagItems  from "./BagItems";
 import { CheckoutBtn } from "./CheckoutBtn";
 import { ItemBox } from "./ItemBox";
-import  OrderSummary  from "./OrderSummary";
+import  {OrderSummary}  from "./OrderSummary";
 import { OrderSummaryDataSection } from "./OrderSummaryDataSection";
 
 export {
